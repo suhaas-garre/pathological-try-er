15ef46762583989ab7f9ebc47320940950402746
suhaas-garre <suhaas.garre@gmail.com>
2019-09-12 15:38:08 -0400

    writing main.py file to set up eventual training process

--- data.py
+++ data.py
@@ -11,7 +11,6 @@ import random
 
 IMG_WIDTH = 1340
 IMG_HEIGHT = 1004
-BATCH_SIZE = 5
 
 Tumor = []
 Lymphocytes = []
@@ -20,3 +19,36 @@ Stroma = []
 Epith = []
 
 COLOR_DICT = np.array([Tumor, Lymphocytes, Macrophages, Stroma, Epith])
+
+def trainGenerator(batch_size, train_path,image_folder, mask_folder, aug_dict, image_color_mode = "grayscale",
+                     mask_color_mode = "grayscale", image_save_prefix  = "image", mask_save_prefix  = "mask",
+                     flag_multi_class = False, num_class = 2, save_to_dir = None, target_size = (512,512), seed = 1):
+
+	img_datagen = ImageDataGenerator(**aug_dict)
+	mask_datagen = ImageDataGenerator(**aug_dict)
+
+	img_generator = img_datagen.flow_from_directory(
+		train_path,
+		classes = [image_folder], 
+		class_mode = None,
+		color_mode = image_color_mode,
+		target_size = target_size, 
+		batch_size = batch_size, 
+		save_to_dir = save_to_dir, 
+		save_prefix = image_save_prefix,
+		seed = seed)
+	mask_generator = mask_datagen.flow_from_directory(
+		train_path,
+		classes = [mask_folder], 
+		class_mode = None,
+		color_mode = mask_color_mode,
+		target_size = target_size, 
+		batch_size = batch_size, 
+		save_to_dir = save_to_dir, 
+		save_prefix = mask_save_prefix,
+		seed = seed)
+	
+	train_generator = zip(img_generator, mask_generator)
+	for (img, mask) in train_generator:
+		img, mask = adjustData(img, mask, flag_multi_class, num_class)
+		yield (img, mask)
--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from unet_model import *
 from data import *
 
+TRAINPATH = '/Users/suhaas/Desktop/stott_lab/HN_Segmentation'
 
+aug_gen_param = dict(rotation_range=2,
+					width_shift_range=0.05,
+					height_shift_range=0.05, 
+					shear_range=0.05,
+					zoom_range=0.05,
+					horizontal_flip=True, 
+					vertical_flip=True,
+					fill_mode='reflect')
+
+training_gen = trainGenerator(5, TRAINPATH, 'image', 'mask', aug_gen_param, save_to_dir = None)
